9ca1c05c44d6e558e329a76c641ab7c5b4e0d566
Antonius Helmer <114677535+MikisugiV@users.noreply.github.com>
2026-02-20 13:55:53 +0100

    camera functionalities
    simplified camera functionalities for robotDK

---
+++ Python/camera.py
@@ -0,0 +1,243 @@
+import math
+import cv2
+import numpy as np
+from vmbpy import *
+from pyzbar.pyzbar import decode, ZBarSymbol
+
+DEBUG_CODE = False                               # true if you want prints, false if you don't
+QR_REAL_SIZE = 40                               # in milimeters
+QR_PIXEL_SIZE = 124                             # in pixels
+REAL_PIXEL_SIZE = QR_REAL_SIZE/QR_PIXEL_SIZE    # 1 pixel in mm
+OFFSET_X = 10                                   # in pixels
+OFFSET_Y = 10                                   # in pixels
+OFFSET_ANGLE = 1                                # in degrees
+MIDDLE_POINT_CAMERA = [640, 512]                # in pixels
+
+# function to calibrate the move commandos according to an QRcode
+# it is expected that the robot has positioned the camera above the QRcode
+# function that will return the position of the qr code compared to the camera
+def calibrate_robot():
+    
+    # take picture and return the QR position
+    camera_present, my_movement = get_picture()
+    if camera_present == False:
+        if DEBUG_CODE:
+            print("There is no camera present")
+        return False, [0,0,0]
+    else:
+        my_movement[0] = my_movement[0]/1000
+        my_movement[1] = my_movement[1]/1000
+        return True, my_movement
+
+# function to get a picture
+def get_picture():
+    with VmbSystem.get_instance() as vmb:
+        cams = vmb.get_all_cameras()
+
+        # check if there is a camera
+        if not cams:
+            if DEBUG_CODE:
+                print("No Cameras found.")
+            return False, [0, 0, 0]
+
+        with cams[0] as cam:
+            if DEBUG_CODE:
+                print(f"Accessed Camera: {cam.get_id()}")
+
+            state_camera = setup_camera(cam)
+            
+            if not state_camera:
+                if DEBUG_CODE:
+                    print(f"State of the Camera: {state_camera}")
+                return False, [0, 0, 0]
+
+            try:
+                frame = cam.get_frame()
+
+                # 1. Access Raw Numpy Array
+                img_buffer = frame.as_numpy_ndarray()
+
+                # 2. Convert BayerRG -> BGR (As confirmed working)
+                img_color = cv2.cvtColor(img_buffer, cv2.COLOR_BayerRG2BGR)
+
+                # 3. Calculate Pose
+                _movement = get_camera_pose(img_color, True)
+                if DEBUG_CODE:
+                    print("get picture movement: ", _movement)
+
+                return True, _movement
+
+            except Exception as e:
+                print(f"Error: {e}")
+                return False, e
+
+# initialize the camera with the correct settings
+def setup_camera(_cam):
+    try:
+        _cam.ExposureAuto.set('Off')        # turn off auto exposure
+        _cam.ExposureTimeAbs.set(80000)     # set exposure time to 80000 micros seconds
+        _cam.Gain.set(3)                    # set gain to 3
+        return True
+    except:
+        return False
+
+# get the location of the camera compared to the QR code
+def get_camera_pose(frame):
+    # initialize variable that will return data
+    movement = [0, 0, 0]    #[x, y, rz]
+
+    # make frame grey scale
+    if len(frame.shape) == 3:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = frame
+
+    # equalize frame
+    equ = cv2.equalizeHist(gray)
+
+    # look for QRcodes in the frame
+    decoded_objects = decode(equ, symbols=[ZBarSymbol.QRCODE])
+
+    # check if all found objects are the correct qr codes
+    for obj in decoded_objects:
+        # Extract the corner points
+        points = obj.polygon
+
+        # We need exactly 4 corners for PnP
+        if len(points) == 4:
+            # make it an numpy array
+            image_points = np.array(points, dtype=np.float32)
+            # draw the shape around the QRcode (is also used for different things)
+            pts_draw = image_points.astype(int).reshape((-1, 1, 2))
+            cv2.polylines(frame, [pts_draw], True, (0, 255, 0), 3)
+
+            # in pts_draw the first variable is X and the second is Y
+            # it seems the order in which the points are listed in the array change.
+            # it seems the firs point in the array is always the most left point and the rest follows counter clock wise
+
+            # find the middle point of the QRcode
+            x_ave = (pts_draw[0][0][0] + pts_draw[1][0][0] + pts_draw[2][0][0] + pts_draw[3][0][0]) / 4
+            y_ave = (pts_draw[0][0][1] + pts_draw[1][0][1] + pts_draw[2][0][1] + pts_draw[3][0][1]) / 4
+
+            # initialize the points with a standard number
+            point1 = 1111
+            point2 = 1111
+            point3 = 1111
+            point4 = 1111
+
+            # arrange the points in an predetermind order
+            for i in pts_draw:
+                if i[0][1] < y_ave:
+                    if i[0][0] < x_ave:
+                        point1 = [int(i[0][0]), int(i[0][1])]
+                    else:
+                        point2 = [int(i[0][0]), int(i[0][1])]
+                else:
+                    if i[0][0] > x_ave:
+                        point3 = [int(i[0][0]), int(i[0][1])]
+                    else:
+                        point4 = [int(i[0][0]), int(i[0][1])]
+
+            organized_points = [point1, point2, point3, point4]     # LT, RT, RB, LB
+
+            # find orientation (it should never happen, but the QRcode could be orientated more then 20 degrees)
+            x_axis = math.fabs(organized_points[3][0] - organized_points[0][0])
+            y_axis = math.fabs(organized_points[3][1] - organized_points[0][1])
+
+            if x_axis == 0:
+                tan_angle = 90
+                degrees_angle = 90
+            else:
+                tan_angle = math.atan(y_axis / x_axis)
+                degrees_angle = math.degrees(tan_angle)
+
+            if organized_points[0][0] < organized_points[3][0]:
+                oneEight = "left"
+            else:
+                oneEight = "right"
+
+            turn_direction = ""
+            if DEBUG_CODE:
+                print(obj.orientation)
+
+            if obj.orientation == "RIGHT":
+                turn_direction = "counter clockwise"
+                if oneEight == "left":
+                    dif_angle = degrees_angle
+                else:
+                    dif_angle = (90 - degrees_angle) + 90
+            elif obj.orientation == "LEFT":
+                turn_direction = "clockwise"
+                if oneEight == "right":
+                    dif_angle = degrees_angle
+                else:
+                    dif_angle = (90 - degrees_angle) + 90
+            elif obj.orientation == "DOWN":
+                dif_angle = degrees_angle + 90
+                if oneEight == "left":
+                    turn_direction = "counter clockwise"
+                else:
+                    turn_direction = "clockwise"
+            else:
+                dif_angle = 90 - degrees_angle
+                if oneEight == "left":
+                    turn_direction = "counter clockwise"
+                else:
+                    turn_direction = "clockwise"
+
+            # calculate the X and Y position on the camera image
+            x_mid_point = organized_points[0][0] + ((organized_points[2][0] - organized_points[0][0]) / 2)
+            y_mid_point = organized_points[0][1] + ((organized_points[2][1] - organized_points[0][1]) / 2)
+
+            # calculate the X and Y offset compared to the middlepoint of the camera
+            difX = MIDDLE_POINT_CAMERA[0] - x_mid_point  # positive means the qr is left to the camera middle point
+            difY = MIDDLE_POINT_CAMERA[1] - y_mid_point  # positive means the qr is above to the camera middle point
+
+            # check if the robot should move in the X direction
+            if x_mid_point < (MIDDLE_POINT_CAMERA[0] + OFFSET_X) and x_mid_point > (
+                    MIDDLE_POINT_CAMERA[0] - OFFSET_X):
+                if DEBUG_CODE:
+                    print(f"The QR is within {OFFSET_X * REAL_PIXEL_SIZE} mm of the centre of the camera in the X direction, fault is {difX} pixels")
+                # this value means the camera is within the expected offset
+                movement[0] = 0.000000001
+            elif difX < 0:
+                if DEBUG_CODE:
+                    print(f"Please move the camera {difX * REAL_PIXEL_SIZE} mm to the left")
+                movement[0] = abs(difX * REAL_PIXEL_SIZE)
+            else:
+                if DEBUG_CODE:
+                    print(f"Please move the camera {difX * REAL_PIXEL_SIZE} mm to the right")
+                movement[0] = -1 * abs(difX * REAL_PIXEL_SIZE)
+            
+            # check if the robot should move in the Y direction
+            if y_mid_point < (MIDDLE_POINT_CAMERA[1] + OFFSET_Y) and y_mid_point > (
+                    MIDDLE_POINT_CAMERA[1] - OFFSET_Y):
+                if DEBUG_CODE:
+                    print(f"The QR is within {OFFSET_Y * REAL_PIXEL_SIZE} mm of the centre of the camera in the Y direction, fault is {difY} pixels")
+                # this value means the camera is within the expected offset
+                movement[1] = 0.000000001
+            elif difY < 0:
+                if DEBUG_CODE:
+                    print(f"Please move the camera {difY * REAL_PIXEL_SIZE} mm up")
+                movement[1] = abs(difY * REAL_PIXEL_SIZE)
+            else:
+                if DEBUG_CODE:
+                    print(f"Please move the camera {difY * REAL_PIXEL_SIZE} mm down")
+                movement[1] = -1 * abs(difY * REAL_PIXEL_SIZE)
+
+            # check if the robot should move around it's base
+            if dif_angle < (0 + OFFSET_ANGLE) and dif_angle > (0 - OFFSET_ANGLE):
+                if DEBUG_CODE:
+                    print(f"The QR is within {OFFSET_ANGLE} degrees of the orientation of the camera in the Rz direction, fault is {dif_angle} degrees")
+                # this value means the camera is within the expected offset
+                movement[2] = 0.000000001
+            else:
+                if DEBUG_CODE:
+                    print(f"Please turn the camera {dif_angle} degrees {turn_direction}")
+                if turn_direction == "counter clockwise":
+                    movement[2] = -1 * math.radians(dif_angle)
+                else:
+                    movement[2] = math.radians(dif_angle)
+    if DEBUG_CODE:
+        print("get camera movement: ", movement)
+    return movement
